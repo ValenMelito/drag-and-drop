b557321a55c29332d6134e707745202fa34f70c9
Valentin Melito <vmelito686@gmail.com>
2024-03-05 23:35:40 -0300

    aprendiendo con la funcion de lectura de mouse para hacer el arrastre

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ function agregarNota(){
     let contenidoNota = document.getElementById('nota');
     console.log(contenidoNota.value)
 
+    if(contenidoNota.value==""){
+        contenidoNota.focus();
+    }
+
     if(contenidoNota.value!=""){
         interiorNota=contenidoNota.value;
         let nuevoElemento = document.createElement('div');
@@ -22,7 +26,7 @@ function agregarNota(){
         nuevoElemento.textContent=contenidoNota.value;
         nuevoElemento.onclick=cambiarFondo;
 
-        let posicion=document.getElementById('sectorDeNotas');
+        let posicion=document.getElementById('centrado');
 
         posicion.appendChild(nuevoElemento);
 
@@ -32,6 +36,27 @@ function agregarNota(){
 }
 
 
+/*document.addEventListener('mousedown',mover(e))
+
+    function mover(e){
+        let posicionX = e.clientX - offsetX + 'px';
+        let posicionY = e.clientY - offsetY + 'px';
+
+        console.log("posicion del mouse en X: "+posicionX+" y posicion Y:"+posicionY) 
+    }*/
+
+    
+ document.addEventListener('mousedown', (posicion) => {
+            const mouseX = posicion.clientX;
+            const mouseY = posicion.clientY;
+            //console.clear(); // Limpiar la consola para una salida más limpia
+            console.log(`Coordenadas del mouse: x=${mouseX}, y=${mouseY}`);
+        });
+/*
+document.addEventListener('keydown', (e) => {
+    const teclaPresionada= e.key;
+    console.log(teclaPresionada);
+})*/
 
    function a(){
     console.log("aaaa")
@@ -42,7 +67,5 @@ function agregarNota(){
         let numeroRandom=Math.floor(Math.random() * 999999);
         console.log(numeroRandom);
         fondo.style.backgroundColor = `#${numeroRandom}`;
-
-
        
     }
